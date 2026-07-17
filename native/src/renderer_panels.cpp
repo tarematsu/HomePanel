15b113f642e17b70f3c8ccc1161aa5185dcc272f
tarematsu <yuukiar0202@gmail.com>
2026-07-17 10:03:51 +0900

    Apply rearranged panel layout

--- native/src/renderer_panels.cpp
+++ native/src/renderer_panels.cpp
@@ -1,7 +1,20 @@
 // Kept as one translation unit so cached GDI primitives remain shared.
 // Fragment boundaries follow complete responsibilities and never split functions.
 #include "renderer_panels/primitives.inc"
+#include "renderer_panels/layout_overrides.inc"
+
+#define SplitSidebarSections SplitRearrangedSidebarSections
+#define SplitMainSections SplitRearrangedMainSections
+#define ControlsFromContent RearrangedControlsFromContent
+#define DrawClockSection HP_DRAW_CLOCK_WITH_CONTROLS
+#define DrawControlsSection DrawAirSection
 #include "renderer_panels/windows.inc"
+#undef DrawControlsSection
+#undef DrawClockSection
+#undef ControlsFromContent
+#undef SplitMainSections
+#undef SplitSidebarSections
+
 #include "renderer_panels/environment_sections.inc"
 #include "renderer_panels/media_section.inc"
 #include "renderer_panels/data_sections.inc"
