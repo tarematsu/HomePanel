a91284c9fb07c8fac4a0fc44675e9e78abca0b0a
tarematsu <yuukiar0202@gmail.com>
2026-07-19 11:59:45 +0900

    Track Stationhead history persistence

--- native/src/app.h
+++ native/src/app.h
@@ -98,6 +98,7 @@ class App {
   uint64_t stationheadPlaybackFallbackRevision_ = 0;
   int64_t lastTelemetryAt_ = 0;
   int64_t lastStationheadPlayStatsUpdatedAt_ = 0;
+  int64_t lastStationheadPlayHistorySavedAt_ = 0;
   int64_t toastUntil_ = 0;
   int64_t nextAppTickAt_ = 0;
   int newsIndex_ = 0;
